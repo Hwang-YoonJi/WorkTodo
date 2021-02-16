85bed104b215ee19e518ecd8ce423711aeac6c10
Hwang-yoonji <kaci00@naver.com>
2021-02-16 18:51:11 +0900

    WorkTodo

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView,AsyncStorage } from 'react-native';
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import TaskModal from './components/TaskModal'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    AsyncStorage.getItem('@todo:state').then((state) => {
+      this.setState(JSON.parse(state))
+    })
+  }
+
+  state = {
+    todos: [
+      {
+        title: "일기쓰기",
+        done: true,
+      },
+      {
+        title: "스터디 준비하기",
+        done: false,
+      },
+    ],
+    showModal: false,
+  }
+
+  save = () => {
+    AsyncStorage.setItem('@todo:state', JSON.stringify(this.state))
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header 
+          show={() => {
+            this.setState({ showModal: true})
+          }}
+        />
+        <FlatList
+          data={this.state.todos}
+          renderItem={({ item, index }) => {
+            return (
+              <TodoItem
+                title={item.title}
+                done={item.done}
+                keyExtractor={(id, index) => {
+                  return id + '${index}'
+                }}
+                remove={() => {
+                  this.setState({
+                    todos: this.state.todos.filter((_, i) => i !== index)
+                  }, this.save)
+                }}
+                toggle={() => {
+                  const newTodos = [...this.state.todos]
+                  newTodos[index].done = !newTodos[index].done
+                  this.setState({todos: newTodos}, this.save)
+                }}
+              />
+            )
+          }}
+        />
+        <TaskModal 
+          isVisible={this.state.showModal}
+          add={(title) => {
+            this.setState({
+              todos: this.state.todos.concat({
+                title: title,
+                done: false,
+              }),
+              showModal: false,
+            }, this.save)
+          }}
+          hide={() => {
+            this.setState({ showModal : false })
+          }} 
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
